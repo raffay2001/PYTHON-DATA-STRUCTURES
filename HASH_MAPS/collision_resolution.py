03eead4ba375baac89b05cbf97a4a285fe10027d
Abdul Raffay <raffay.ahmed009@gmail.com>
2022-01-10 10:35:57 +0500

    fifth commit

---
+++ HASH_MAPS/collision_resolution.py
@@ -0,0 +1,40 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
+    
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        found = False
+        for index, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][index] = (key, value)
+                found = True
+        if not found:
+            self.arr[h].append((key, value))
+
+    
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+    
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        for index, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                print(f'Deleting {element}')
+                del self.arr[h][index]
+    
+
+t = HashTable()
+t['march 6'] = 129.0
+t['march 17'] = 2222.0
+print(t.arr)
